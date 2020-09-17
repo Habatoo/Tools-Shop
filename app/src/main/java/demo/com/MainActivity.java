422f9acc3e14a67967558614b01a4cf8d9fecbef
Habatoo <habatoo@gmail.com>
2020-09-17 21:45:23 +0700

    final commit add all menu

--- app/src/main/java/demo/com/MainActivity.java
+++ app/src/main/java/demo/com/MainActivity.java
@@ -1,14 +1,48 @@
 package demo.com;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewTools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar(); // получаем ссылку на action bar
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        listViewTools = findViewById(R.id.listViewTools);
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Toast.makeText(getApplicationContext(), "Позиция: " + position, Toast.LENGTH_SHORT).show();
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), DrillCategoryActivity.class);
+                        startActivity(intent);
+                        break;
+//                    case 1:
+//                        Intent intent = new Intent(getApplicationContext(), DrillCategoryActivity.class);
+//                        startActivity(intent);
+//                        break;
+//                    case 2:
+//                        Intent intent = new Intent(getApplicationContext(), DrillCategoryActivity.class);
+//                        startActivity(intent);
+//                        break;
+                }
+            }
+        }); // слушатель кликов на элементы списка
     }
 }
